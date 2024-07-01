e6f2d58e4883d55901e8c280d65f8b79d2435e35
Ken Udoudo <kendo302003@gmail.com>
2024-07-01 12:09:44 -0700

    js working

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function updateTime() {
+    const now = new Date();
     const hours = now.getUTCHours().toString().padStart(2, '0');
     const minutes = now.getUTCMinutes().toString().padStart(2, '0');
+    const day = now.getUTCDate().toString().padStart(2, '0');
 
     const utcTimeString = `${hours}:${minutes} UTC`;
     document.getElementById('currentTimeUTC').textContent = utcTimeString;
+    document.getElementById('curentDay').textContent = day;
 }
+
+updateTime();
+setInterval(updateTime, 1000);
